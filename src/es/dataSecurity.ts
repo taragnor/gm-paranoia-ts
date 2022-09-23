df5df1a0b80ac8ba32a3d5ce948cf723fa7e074a
Joe Martino <taragnor@yahoo.com>
2022-09-23 08:42:40 -0400

    starting work on saving list of changes

--- src/es/dataSecurity.ts
+++ src/es/dataSecurity.ts
@@ -3,6 +3,7 @@ interface Changes {
 	oldValue: any;
 	key: string;
 	newValue: any;
+	__isChanges: boolean;
 }
 
 interface ChangeLog {
@@ -14,24 +15,26 @@ interface ChangeLog {
 	};
 }
 
+type RecursiveArray<T> = (T | T[])[];
+
 export class DataSecurity {
+	static logFilePath : string = "";
 
 	static init() {
 		// Hooks.on("updateActor", this.onActorUpdate.bind(this));
 		Hooks.on("preUpdateActor", this.onActorPreUpdate.bind(this));
 	}
 
-
 	static onActorPreUpdate( actor: Actor, changes: ChangeLog, _options: {}, userId:string  ) {
 		const oldS = actor.system;
 		const newS = changes.system;
 		const list = this.iterateObject(oldS, newS, userId);
 		console.log("Update");
-		console.log(arguments);
 		console.log(list);
+		this.storeChanges(list);
 	}
 
-	static iterateObject ( oldData : ArbitraryObject, newData: ArbitraryObject, playerId: string) : Changes[] {
+	static iterateObject ( oldData : ArbitraryObject, newData: ArbitraryObject, playerId: string) : RecursiveArray<Changes> {
 		return Object.entries(newData)
 			.map( ([key, val]) => {
 				const oldval = oldData[key];
@@ -43,11 +46,23 @@ export class DataSecurity {
 						key,
 						oldValue: oldval,
 						newValue: val,
+						__isChanges: true,
 					};
 				}
 			})
 		.flat(1);
 	}
+
+	static isChanges(x: ArbitraryObject) : x is Changes {
+		if (x.__isChanges) return true;
+		return false;
+	}
+
+	static storeChanges(list: RecursiveArray<Changes>) : void {
+		//TODO: open log file, save to thing
+
+	}
+
 }
 
 
