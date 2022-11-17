175f48b1815686747e63b9380b1a7b6b0c74ad6e
Joe Martino <taragnor@yahoo.com>
2022-11-17 01:48:22 -0500

    dialog fixups

--- src/es/changelog-dialog.ts
+++ src/es/changelog-dialog.ts
@@ -1,16 +1,48 @@
 import {ChangeLogger} from "./dataSecurity.js";
 
-class ChangelogDialog extends Dialog {
-	async refreshContent() {
-		const e = $(this.element);
-		e.empty();
+export class ChangelogDialog extends Dialog {
+
+	constructor () {
+		super( {
+			title: "Change Log",
+			content: "",
+			close: () =>  {},
+			render: () => {
+				this.onRender();
+			},
+			buttons: {
+				close: {
+					label: "close",
+					icon: "close",
+					callback: () => {},
+				},
+			}
+		});
+	}
+
+	static create() {
+		const dialog = new ChangelogDialog();
+		dialog.render(true);
+		return dialog;
+	}
+
+	async onRender() {
+		this.refreshContent();
+	}
+
+	async getContent() {
 		const templateData = {
 			log: ChangeLogger.log
 		};
 		const html = await renderTemplate("modules/gm-paranoia-taragnor/hbs/change-log.hbs", templateData);
+		return html;
+	}
+
+	async refreshContent() {
+		const e = $(this.element);
+		e.empty();
+		const html = await this.getContent();
 		e.append(html);
-
-
 	}
 
 
