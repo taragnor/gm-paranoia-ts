48e8643d64458505f87543e5dddb50bafef54f3f
Joe Martino <taragnor@yahoo.com>
2024-05-23 13:32:22 -0400

    inline rolsl work?

--- src/es/roller-patch.ts
+++ src/es/roller-patch.ts
@@ -33,16 +33,16 @@ export function applyPatch() {
 	const _oldPreCreate = ChatMessage.prototype._preCreate;
 	ChatMessage.prototype._preCreate = async function (...args: any[]) {
 		anchoredRolls.rolls= [];
-		await _oldPreCreate.apply(this, args);
-		debugger;
+		// debugger;
 		const data = args[0];
+		await _oldPreCreate.apply(this, args);
 		if (anchoredRolls.rolls.length> 0) {
 			if ("rolls" in data) {
 				data.rolls.concat(anchoredRolls.rolls);
-				await this.updateSource(data);
+				await this.updateSource({rolls: data.rolls});
 			} else {
 				data.rolls = anchoredRolls.rolls;
-				await this.updateSource(data);
+				await this.updateSource({rolls: data.rolls});
 			}
 			anchoredRolls.rolls= [];
 		}
@@ -52,6 +52,7 @@ export function applyPatch() {
 	Roll.prototype.toAnchor = function (...args: unknown[]): HTMLElement {
 		if (!anchoredRolls.rolls.includes(this)) {
 			anchoredRolls.rolls.push(this);
+			Debug(this);
 		}
 		return _oldtoAnchor.apply(this,args);
 	}
